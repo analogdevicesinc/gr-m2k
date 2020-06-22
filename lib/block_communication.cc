791d0679d0fa4a8d6cdc411cbb1b8d135fbafda7
DanielGuramulta <Daniel.Guramulta@analog.com>
2020-06-22 12:33:59 +0300

    WIP
    Signed-off-by: DanielGuramulta <Daniel.Guramulta@analog.com>

--- lib/block_communication.cc
+++ lib/block_communication.cc
@@ -24,6 +24,8 @@ void block_communication::request_sync_between_analog_digital(const std::string 
 		d_buffer_size = buffer_size;
 		libm2k::context::M2k *context = gr::m2k::analog_in_source_impl::get_context(uri);
 		context->startMixedSignalAcquisition(d_buffer_size);
+		d_current_buffers_captured[uri] = {0, 0};
+		d_kernel_buffers_full[uri] = false;
 	}
 }
 
@@ -49,20 +51,34 @@ bool block_communication::can_capture(const std::string &uri, block_communicatio
 	}
 }
 
-void block_communication::data_captured(const std::string &uri, block_communication::SYNC_DEVICE device)
+void block_communication::data_captured(const std::string &uri, block_communication::SYNC_DEVICE device, int device_kernel_buffers)
 {
 	std::unique_lock<std::mutex> d_lock(d_sync_state_mutex);
 	switch (device) {
 	case SYNC_DEVICE::ANALOG:
 		d_sync_state[uri].first = false; // no longer capturing on analog
+		d_current_buffers_captured[uri].first = d_current_buffers_captured[uri].first++;
+		if (device_kernel_buffers == d_current_buffers_captured[uri].first) {
+			d_kernel_buffers_full[uri] = true;
+		}
 	case SYNC_DEVICE::DIGITAL:
 		d_sync_state[uri].second = false; // no longer capturing on digital
+		d_current_buffers_captured[uri].second = d_current_buffers_captured[uri].second++;
+		if (device_kernel_buffers == d_current_buffers_captured[uri].second) {
+			d_kernel_buffers_full[uri] = true;
+		}
 	}
 
 	if (!d_sync_state[uri].first && !d_sync_state[uri].second) {
-		libm2k::context::M2k *context = gr::m2k::analog_in_source_impl::get_context(uri);
-		context->startMixedSignalAcquisition(d_buffer_size);
 		d_sync_state[uri] = {true, true}; // reset state, can capture on both digital and analog
+		if (d_kernel_buffers_full[uri]) {
+			// reached kernel buffers limit for analog / digital
+			d_kernel_buffers_full[uri] = false;
+			d_current_buffers_captured[uri] = {0, 0};
+			libm2k::context::M2k *context = gr::m2k::analog_in_source_impl::get_context(uri);
+			context->stopMixedSignalAcquisition();
+			context->startMixedSignalAcquisition(d_buffer_size); // d_buffer_size -> d_buffer_size[uri]
+		}
 	}
 }
 
--- lib/block_communication.h
+++ lib/block_communication.h
@@ -19,7 +19,7 @@ public:
 	void request_sync_between_analog_digital(const std::string &uri, bool sync, int buffer_size);
 	bool is_sync_requested(const std::string &uri) const;
 	bool can_capture(const std::string &uri, SYNC_DEVICE device);
-	void data_captured(const std::string &uri, SYNC_DEVICE device);
+	void data_captured(const std::string &uri, SYNC_DEVICE device, int device_kernel_buffers);
 
 public: /* deleted functions */
 	block_communication(block_communication const &) = delete;
@@ -31,7 +31,9 @@ private:
 private:
 	std::map<std::string, bool> d_sync_requested;
 	std::map<std::string, std::pair<bool, bool>> d_sync_state;
-	mutable std::mutex d_sync_request_mutex;
+	std::map<std::string, std::pair<int, int>> d_current_buffers_captured;
+	std::map<std::string, bool> d_kernel_buffers_full;
+	mutable std::mutex d_sync_request_mutex; // [uri]
 	mutable std::mutex d_sync_state_mutex;
 	int d_buffer_size;
 };
